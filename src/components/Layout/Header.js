971f818a53629e5dc8e795a2f4182e208d473b04
Corri <95197153+corricodes@users.noreply.github.com>
2023-02-16 12:12:55 -0500

    added styling and components for menu items

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = props => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>Healthy Meals</h1>
+            <HeaderCartButton />
+        </header>
+        <div className={classes['main-image']}>
+           <img src={mealsImage} alt="Healthy food on cutting board." /> 
+        </div>
+    </Fragment>
+};
+
+export default Header;
