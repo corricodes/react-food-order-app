a135fa850285f8812a8c1930af02f66f3f2af4d4
Corri <95197153+corricodes@users.noreply.github.com>
2023-06-02 13:13:23 -0400

    added show cart and close cart handlers

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,21 @@
 import { Fragment } from "react";
 
 import HeaderCartButton from "./HeaderCartButton";
-import mealsImage from '../../assets/meals.jpg';
-import classes from './Header.module.css';
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 
-const Header = props => {
-    return <Fragment>
-        <header className={classes.header}>
-            <h1>Healthy Meals</h1>
-            <HeaderCartButton />
-        </header>
-        <div className={classes['main-image']}>
-           <img src={mealsImage} alt="Healthy food on cutting board." /> 
-        </div>
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Healthy Meals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="Healthy food on cutting board." />
+      </div>
     </Fragment>
+  );
 };
 
 export default Header;
